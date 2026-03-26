59c9a7e7349101dc39e78e166ed07a74e236b17f
shahnawaz khan <khan.dev006@gmail.com>
2026-03-26 14:34:31 +0530

    allowed all usered to req to backend for temporary

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -6,7 +6,8 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"],
+    origin: "*",
+    methods: ["GET", "POST"],
   },
 });
 const onlineUsers = new Map(); //userId: socketId
